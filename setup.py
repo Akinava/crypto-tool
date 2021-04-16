fd06cbac9451991cb3285b3e0f1776e3267760f2
Andrei Chernov <Andrei.Chernov1@thomsonreuters.com>
2021-04-16 23:25:05 +0200

    version 0.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='crypto-tool',
     description='cryptography tools',
     url='https://github.com/Akinava/crypto-tool',
-    version='0.0.0',
+    version='0.0.1',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     python_requires='>=3.6, <4',
